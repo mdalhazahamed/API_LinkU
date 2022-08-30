1575e631c063067c195a17dfac8a8ee3b812dc4e
mdalhazahamed <alhazbci99@mail.com>
2022-08-30 11:48:05 +0600

    add

--- lib/api_link_up.dart
+++ lib/api_link_up.dart
@@ -2,7 +2,6 @@ import 'package:api/moldel/comments.dart';
 import 'package:api/moldel/comments_helper.dart';
 import 'package:flutter/material.dart';
 
-
 class ApiLin_Up extends StatefulWidget {
   @override
   State<ApiLin_Up> createState() => _ApiLin_UpState();
@@ -31,9 +30,8 @@ class _ApiLin_UpState extends State<ApiLin_Up> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: Center(child: Text("API Link-Up") ),
+        title: Center(child: Text("API Link-Up")),
         leading: Icon(Icons.api),
-        
       ),
       backgroundColor: Colors.grey,
       body: Visibility(
@@ -47,44 +45,78 @@ class _ApiLin_UpState extends State<ApiLin_Up> {
                     padding: const EdgeInsets.symmetric(
                         horizontal: 10, vertical: 10),
                     child: Container(
-                      height: 150,
+                      height: 210,
                       width: double.infinity,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(30),
                       ),
-                      
                       child: SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
                         child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 15),
                           child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Row(
                                 children: [
+                                  Text("PostId: "),
                                   Text(
-                                    comments![index].name,
+                                    comments![index].postId.toString(),
                                     style: TextStyle(
-                                      color: Color.fromARGB(255, 5, 49, 145),
+                                      color: Colors.red,
                                       fontSize: 15,
                                     ),
                                   ),
                                 ],
                               ),
-                               Row(
+                              Row(
                                 children: [
+                                  Text("Id: - "),
                                   Text(
-                                    comments![index].email,
+                                    comments![index].id.toString(),
                                     style: TextStyle(
-                                      color: Colors.redAccent,
-                                      fontSize: 12,
+                                      color: Colors.green,
+                                      
+                                      fontSize: 20,
                                     ),
                                   ),
                                 ],
                               ),
-                              SizedBox(height: 20,),
-                               Row(
+                              SizedBox(
+                                height: 10,
+                              ),
+                             Row(
                                 children: [
+                                  Text("Name: "),
+                                  Text(
+                                    comments![index].name,
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("Eamil: "),
+                                  Text(
+                                    comments![index].email,
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                children: [
+                                  
                                   Text(
                                     comments![index].body,
                                     style: TextStyle(
@@ -92,9 +124,9 @@ class _ApiLin_UpState extends State<ApiLin_Up> {
                                       fontSize: 12,
                                     ),
                                   ),
+                                  
                                 ],
                               ),
-                              
                             ],
                           ),
                         ),
